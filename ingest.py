6c0b6a0a75581db4c6094aef0429ce9919e8f194
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-05-17 18:50:01 +0300

    all good investigate occasion memory error on multiple queries
    UserWarning: resource_tracker: There appear to be 1 leaked semaphore objects to clean up at shutdown
      warnings.warn('resource_tracker: There appear to be %d '

--- ingest.py
+++ ingest.py
@@ -3,33 +3,49 @@ from langchain.vectorstores import Chroma
 import time
 
 from settings import SOURCE_DIRECTORY, load_documents, PERSIST_DIRECTORY, CHROMA_SETTINGS, load_embeddings_model
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
-def run():
+class Ingestor:
 
-    start_time = time.time()
+    @staticmethod
+    def split_documents():
 
-    # Load documents and split in chunks
-    print(f"Loading documents from {SOURCE_DIRECTORY}")
+        # Load documents and split in chunks
+        logger.info(f"Loading documents from {SOURCE_DIRECTORY}")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    texts = text_splitter.split_documents(load_documents(SOURCE_DIRECTORY))
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        documents = load_documents(SOURCE_DIRECTORY)
+        texts = text_splitter.split_documents(documents)
 
-    print(f"Loaded {len(list(load_documents(SOURCE_DIRECTORY)))} documents from {SOURCE_DIRECTORY}")
-    print(f"Split into {len(texts)} chunks of text (max. 500 tokens each)")
+        logger.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+        logger.info(f"Split into {len(texts)} chunks of text (max. 500 tokens each)")
 
-    embedding_model = load_embeddings_model()
+        return texts
 
-    # Create and store locally vectorstore
-    db = Chroma.from_documents(texts, embedding=embedding_model, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db = None
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    def embeddings_to_vectordb(self):
 
-    print("Fished creating vectorstore from documents.")
-    print(f"Elapsed time: {round(elapsed_time/60)} minutes")
+        start_time = time.time()
+
+        # Load documents and split in chunks
+        texts = self.split_documents()
+
+        # Load embeddings model
+        embedding_model = load_embeddings_model()
+
+        # Create and store locally vectorstore
+        db = Chroma.from_documents(texts, embedding=embedding_model, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
+        db.persist()
+        db = None
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        logger.info("Fished creating vectorstore from documents.")
+        logger.info(f"Elapsed time: {round(elapsed_time/60)} minutes")
 
 
 if __name__ == "__main__":
-    run()
+    ingestor = Ingestor()
+    ingestor.embeddings_to_vectordb()
