401e1be39bb7d835b57735ae021b38b4d8483bc2
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-05-23 13:57:20 +0300

    revert commit

--- ingest.py
+++ ingest.py
@@ -1,94 +1,51 @@
-import os
-import glob
-from typing import List
-from dotenv import load_dotenv
-
-from langchain.document_loaders import (
-    CSVLoader,
-    EverNoteLoader,
-    PDFMinerLoader,
-    TextLoader,
-    UnstructuredEmailLoader,
-    UnstructuredEPubLoader,
-    UnstructuredHTMLLoader,
-    UnstructuredMarkdownLoader,
-    UnstructuredODTLoader,
-    UnstructuredPowerPointLoader,
-    UnstructuredWordDocumentLoader,
-)
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.docstore.document import Document
-from constants import CHROMA_SETTINGS
+import time
+
+from settings import SOURCE_DIRECTORY, load_documents, PERSIST_DIRECTORY, CHROMA_SETTINGS, load_embeddings_model
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
-# Map file extensions to document loaders and their arguments
-LOADER_MAPPING = {
-    ".csv": (CSVLoader, {}),
-    # ".docx": (Docx2txtLoader, {}),
-    ".docx": (UnstructuredWordDocumentLoader, {}),
-    ".enex": (EverNoteLoader, {}),
-    ".eml": (UnstructuredEmailLoader, {}),
-    ".epub": (UnstructuredEPubLoader, {}),
-    ".html": (UnstructuredHTMLLoader, {}),
-    ".md": (UnstructuredMarkdownLoader, {}),
-    ".odt": (UnstructuredODTLoader, {}),
-    ".pdf": (PDFMinerLoader, {}),
-    ".pptx": (UnstructuredPowerPointLoader, {}),
-    ".txt": (TextLoader, {"encoding": "utf8"}),
-    # Add more mappings for other file extensions and loaders as needed
-}
+class Ingestor:
 
+    @staticmethod
+    def split_documents():
 
-load_dotenv()
+        # Load documents and split in chunks
+        logger.info(f"Loading documents from {SOURCE_DIRECTORY}")
 
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        documents = load_documents(SOURCE_DIRECTORY)
+        texts = text_splitter.split_documents(documents)
 
-def load_single_document(file_path: str) -> Document:
-    ext = "." + file_path.rsplit(".", 1)[-1]
-    if ext in LOADER_MAPPING:
-        loader_class, loader_args = LOADER_MAPPING[ext]
-        loader = loader_class(file_path, **loader_args)
-        return loader.load()[0]
+        logger.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+        logger.info(f"Split into {len(texts)} chunks of text (max. 500 tokens each)")
 
-    raise ValueError(f"Unsupported file extension '{ext}'")
+        return texts
 
+    def embeddings_to_vectordb(self):
 
-def load_documents(source_dir: str) -> List[Document]:
-    # Loads all documents from source documents directory
-    all_files = []
-    for ext in LOADER_MAPPING:
-        all_files.extend(
-            glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True)
-        )
-    return [load_single_document(file_path) for file_path in all_files]
+        start_time = time.time()
 
+        # Load documents and split in chunks
+        texts = self.split_documents()
 
-def main():
-    # Load environment variables
-    persist_directory = os.environ.get('PERSIST_DIRECTORY')
-    source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
-    embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
+        # Load embeddings model
+        embedding_model = load_embeddings_model()
 
-    # Load documents and split in chunks
-    print(f"Loading documents from {source_directory}")
-    chunk_size = 500
-    chunk_overlap = 50
-    documents = load_documents(source_directory)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    texts = text_splitter.split_documents(documents)
-    print(f"Loaded {len(documents)} documents from {source_directory}")
-    print(f"Split into {len(texts)} chunks of text (max. {chunk_size} characters each)")
+        # Create and store locally vectorstore
+        db = Chroma.from_documents(texts, embedding=embedding_model, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
+        db.persist()
+        db = None
+        end_time = time.time()
+        elapsed_time = end_time - start_time
 
-    # Create embeddings
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    
-    # Create and store locally vectorstore
-    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db = None
+        logger.info("Fished creating vectorstore from documents.")
+        logger.info(f"Elapsed time: {round(elapsed_time/60)} minutes")
 
 
 if __name__ == "__main__":
-    main()
+    ingestor = Ingestor()
+    ingestor.embeddings_to_vectordb()
